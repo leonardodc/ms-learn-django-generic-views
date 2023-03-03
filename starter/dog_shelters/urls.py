f669b5fcb4d3855da3af59ff3bdc3bd1395c0381
Leonardo Demeterko <leonardo_foz@hotmail.com>
2023-03-03 00:48:09 -0300

    Criando templates e configs

--- starter/dog_shelters/urls.py
+++ starter/dog_shelters/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
+urlpatterns = [ 
     path('', views.shelter_list, name='shelter_list'),
     path('shelter/<int:pk>', views.shelter_detail, name='shelter_detail'),
 
     # TODO: Register detail view
+    path('dog/<int:pk>', views.DogDetailView.as_view(), name='dog_detail'),
 
 
     # TODO: Register create view
--- starter/dog_shelters/views.py
+++ starter/dog_shelters/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from . import models
+from django.views import generic
+
 # TODO: Import generic views
 
 # Create your views here.
@@ -12,3 +14,8 @@ def shelter_detail(request, pk):
     shelter = get_object_or_404(models.Shelter, pk=pk)
     context = {'shelter': shelter}
     return render(request, 'shelter_detail.html', context)
+
+class DogDetailView(generic.DetailView):
+    model = models.Dog
+    template_name = 'dog_detail.html'
+    context_object_name = 'dog'
